6148b442dd4a77bf425a5949ece130a7029cd102
ansy <plere0011@gmail.com>
2026-03-18 17:24:51 +0900

    누락된 북마크 퀴즈 api 추가

--- src/api/BookmarkApi.ts
+++ src/api/BookmarkApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import type { Word } from "../types/Word";
 
 const api = axios.create({
   baseURL: "http://localhost:10000/api", // Spring 서버
@@ -10,4 +11,9 @@ export const registerBookmarkApi = async (wordId: string): Promise<void> => {
 
 export const deleteBookmarkApi = async (wordId: string): Promise<void> => {
   await api.delete(`/bookmarks/${wordId}`);
+};
+
+export const getRandomBookmark = async (size: number=10): Promise<Word[]> => {
+  const response = await api.get<Word[]>(`/bookmarks/random`);
+  return response.data;
 };
